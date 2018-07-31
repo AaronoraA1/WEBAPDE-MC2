b4837db9b5e300e8d470be38a571d752412d5d3e
AaronoraA1 <aaron_fajardo@dlsu.edu.ph>
2018-07-31 18:56:32 +0800

    pushed a few changes

--- scripts.js
+++ scripts.js
@@ -1,31 +1,24 @@
-$(document).ready(function () {
-$("#loginbutton").click(function (e){
-		                               e.preventDefault
-                                    if($("#wrapper").hasClass("toggledregister")){
-		                               $("#wrapper").removeClass("toggledregister")
-                                       $("#wrapper").addClass("toggledlogin")
-                                        }
-                                    else if($("#wrapper").hasClass("toggledlogin")){
-                                        $("#wrapper").removeClass("toggledlogin")
-                                    }
-                                    else{
-                                        $("#wrapper").addClass("toggledlogin")
-                                    }
-                                    
-		                               });
-$("#registerbutton").click(function (e){
-		                               e.preventDefault
-		                               if($("#wrapper").hasClass("toggledlogin")){
-                                           $("#wrapper").removeClass("toggledlogin")
-                                           $("#wrapper").addClass("toggledregister")
-                                       }
-                                        else if($("#wrapper").hasClass("toggledregister")){
-                                        $("#wrapper").removeClass("toggledregister")
-                                    }
-                                       else{
-                                            $("#wrapper").addClass("toggledregister")
-                                       }
-		                               
-                                       });
-            
- })
+$(document).ready(function() {
+	$("#loginbutton").click(function(e) {
+		e.preventDefault
+		if ($("#wrapper").hasClass("toggledregister")) {
+			$("#wrapper").removeClass("toggledregister")
+			$("#wrapper").addClass("toggledlogin")
+		} else if ($("#wrapper").hasClass("toggledlogin")) {
+			$("#wrapper").removeClass("toggledlogin")
+		} else {
+			$("#wrapper").addClass("toggledlogin")
+		}
+	})
+	$("#registerbutton").click(function(e) {
+		e.preventDefault
+		if ($("#wrapper").hasClass("toggledlogin")) {
+			$("#wrapper").removeClass("toggledlogin")
+			$("#wrapper").addClass("toggledregister")
+		} else if ($("#wrapper").hasClass("toggledregister")) {
+			$("#wrapper").removeClass("toggledregister")
+		} else {
+			$("#wrapper").addClass("toggledregister")
+		}
+	})
+});
